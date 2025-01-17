47e3d1f5e974bc67f97cd1f66c86a7c41782a519
li <li@maisiliym.criome.me>
2025-01-17 13:36:33 +0100

    wip: create streamlined `mkAtomicFlake` & separate `mkCustomAtomicFlake`

--- flake.nix
+++ flake.nix
@@ -5,5 +5,6 @@
     core = import ./atom-nix/core/mod.nix;
     importAtom = import ./atom-nix/core/importAtom.nix;
     mkAtomicFlake = import ./legacy-nix/mkAtomicFlake.nix;
+    mkCustomAtomicFlake = import ./legacy-nix/mkCustomAtomicFlake.nix;
   };
 }
--- legacy-nix/mkAtomicFlake.nix
+++ legacy-nix/mkAtomicFlake.nix
@@ -1,71 +1,23 @@
-{
-  manifest,
-  noSystemManifest ? null,
-  inputs ? { },
-  propagate ? false,
-  features ? null,
-  systems ? [
-    "x86_64-linux"
-    "aarch64-linux"
-    "x86_64-darwin"
-    "aarch64-darwin"
-  ],
-}:
-
+flake:
 let
-  npinsSrcs = import ../atom-nix/npins;
-  lib = import (npinsSrcs."nixpkgs.lib" + "/lib");
-  l = lib // builtins;
+  l = builtins;
+  mkCustomAtomicFlake = import ./mkCustomAtomicFlake.nix;
 
-  importAtom = import ../atom-nix/core/importAtom.nix;
+  flakeRootDir = l.readDir flake.outPath;
+  flakeRootFiles = l.attrNames flakeRootDir;
 
-  mkNoSystemAtom = importAtom {
-    inherit inputs propagate features;
-    system = null;
-    _calledFromFlake = true;
-  };
+  matchManifestFile = string: std.match "(^.*@\.toml)" string;
 
-  noSystemAtom =
-    assert !(verifySystemFeature noSystemManifest) || systemEnabledError;
-    mkNoSystemAtom noSystemManifest;
-
-  verifySystemFeature =
-    atomManifest:
+  mkFileMatch =
+    name:
     let
-      atomConfig = l.fromTOML (l.readFile atomManifest);
-      features = atomConfig.features.default or [ ];
+      match = matchManifestFile name;
     in
-    l.elem "system" features;
+    if (match != null) then match else [ ];
 
-  systemEnabledError = abort "No-System atom has `system` feature enabled";
-
-  hasNoSystemAtom = noSystemManifest != null;
-
-  optionalNoSystemAtom = l.optionalAttrs hasNoSystemAtom noSystemAtom;
-
-  transformedAtomFromSystem =
-    system:
-    let
-      mkAtom = importAtom {
-        inherit
-          inputs
-          propagate
-          features
-          system
-          ;
-        _calledFromFlake = true;
-      };
-      evaluatedAtom = mkAtom manifest;
-      mkPerSystemValue = _: value: { ${system} = value; };
-    in
-    l.mapAttrs mkPerSystemValue evaluatedAtom;
-
-  accumulate = accumulator: set: accumulator // set;
-  combineSets = _: sets: l.foldl' accumulate { } sets;
-
-  transformedAtoms = l.map transformedAtomFromSystem systems;
-
-  combinedPerSystemAttributes = l.zipAttrsWith combineSets transformedAtoms;
+  matchedFileNames = concatMap mkFileMatch flakeRootFiles;
 
 in
-optionalNoSystemAtom // combinedPerSystemAttributes
+mkCustomAtomicFlake {
+  inherit manifest noSystemManifest inputs;
+}
