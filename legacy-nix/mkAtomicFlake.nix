c2c41fde4d2dab7620d9d2ce90a48c992156a366
li <li@maisiliym.criome.me>
2025-02-05 11:27:52 +0100

    feat: new, simplified `mkAtomicFlake` implementation

--- legacy-nix/mkAtomicFlake.nix
+++ legacy-nix/mkAtomicFlake.nix
@@ -1,23 +1,8 @@
-flake:
+inputs: manifest:
 let
-  l = builtins;
   mkCustomAtomicFlake = import ./mkCustomAtomicFlake.nix;
-
-  flakeRootDir = l.readDir flake.outPath;
-  flakeRootFiles = l.attrNames flakeRootDir;
-
-  matchManifestFile = string: std.match "(^.*@\.toml)" string;
-
-  mkFileMatch =
-    name:
-    let
-      match = matchManifestFile name;
-    in
-    if (match != null) then match else [ ];
-
-  matchedFileNames = concatMap mkFileMatch flakeRootFiles;
 
 in
 mkCustomAtomicFlake {
-  inherit manifest noSystemManifest inputs;
+  inherit inputs manifest;
 }
