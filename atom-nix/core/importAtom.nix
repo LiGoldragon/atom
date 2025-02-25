cf2fc0dc960469a5f152e4e08003251fe8c420db
li <li@maisiliym.criome.me>
2025-02-25 13:19:55 +0100

    feat!: importAtom major changes Introduced `extern` types Implemented flake extern Implemented atom extern Implemented local atom extern Implemented externs from flake-lock Implemented inputs from arguments

--- atom-nix/core/importAtom.nix
+++ atom-nix/core/importAtom.nix
@@ -18,12 +18,18 @@
 importAtomArgs@{
   system ? null,
   features ? null,
+  inputs ? { },
+  propagate ? false,
+  _calledFromFlake ? false,
   __internal__test ? false,
+  # Passed to further `importAtom`s
+  importAtom ? (import ./importAtom.nix),
+  core ? (import ./mod.nix),
+  flakeCompatFn ? (import (import ../npins).flake-compat),
+  flakeInputsFn ? (import (import ../npins).flake-inputs),
 }:
 path':
 let
-  core = import ./mod.nix;
-
   path = core.prepDir path';
 
   file = builtins.readFile path;
@@ -36,6 +42,9 @@ let
 
   coreConfig = config.core or { };
   std = config.std or { };
+  fetch = config.fetch or { };
+
+  propagate = importAtomArgs.propagate or false || atom.propagate or false;
 
   features =
     let
@@ -61,54 +70,141 @@ let
     # Here, `seq` ensures that `id` is set
     builtins.seq id (atom.src or impliedSrc);
 
-  extern =
+  extern = core.filterMap mkExtern fetch;
+
+  mkExtern =
+    depName: depConfig:
     let
-      fetcher = nix.fetcher or "native"; # native doesn't exist yet
-      throwMissingNativeFetcher = abort "Native fetcher isn't implemented yet";
+      type = depConfig.type or "atom";
+      depIsOptional = depConfig.optional or false;
+      featureIsEnabled = builtins.elem depName features;
+      depIsEnabled = !depIsOptional || (depIsOptional && featureIsEnabled);
+      typeError = abort "Dependency `${depName}` declares type `${type}` which does not exist";
 
-      fetcherConfig = config.fetcher or { };
-      npinRoot = fetcherConfig.npin.root or "npins";
-      rawNpins = import (dirOf path + "/${npinRoot}");
+      makeIndex = {
+        atom = mkAtom;
+        flake = mkFlake;
+        import = mkImport;
+        lib = mkImport;
+        local = mkAtom;
+        src = mkSrc;
+      };
 
-      fetchEnabledNpinsDep =
-        depName: depConfig:
-        let
-          depIsEnabled =
-            (depConfig.optional or false && builtins.elem depName features) || (!depConfig.optional or false);
-
-          npinSrc = "${rawNpins.${depConfig.name or depName}}/${depConfig.subdir or ""}";
-
-          applyArguments =
-            appliedFunction: nextArgument:
-            let
-              argsFromDeps = depConfig.argsFromDeps or true && builtins.isAttrs nextArgument;
-              argIntersectedwithDeps = nextArgument // (builtins.intersectAttrs nextArgument extern);
-            in
-            if argsFromDeps nextArgument then
-              appliedFunction argIntersectedwithDeps
-            else
-              appliedFunction nextArgument;
-
-          dependency =
-            if depConfig.import or false then
-              if depConfig.args or [ ] != [ ] then
-                builtins.foldl' applyArguments (import npinSrc) depConfig.args
-              else
-                import npinSrc
-            else
-              npinSrc;
-        in
-        if depIsEnabled then { "${depName}" = dependency; } else null;
-
-      npinsDeps = core.filterMap fetchEnabledNpinsDep config.fetch or { };
+      dependency = (makeIndex.${type} or typeError) depName depConfig;
 
     in
-    if fetcher == "npins" then
-      npinsDeps
-    else if fetcher == "native" then
-      throwMissingNativeFetcher
-    else
-      { };
+    if depIsEnabled then { "${depName}" = dependency; } else null;
+
+  fetcher = nix.fetcher or "native";
+  fetcherConfig = config.fetcher or { };
+
+  # TODO native doesn't exist yet
+  throwMissingNativeFetcher = abort "Native fetcher isn't implemented yet";
+  throwNonExistingFetcher = abort "A `${backend}` fetcher does not exist";
+
+  npinRoot = fetcherConfig.npin.root or "npins";
+  rawNpins = import (root + "/${npinRoot}");
+  npinsInputs = rawNpins // (importAtomArgs.inputs or { });
+
+  flakeLock = flakeInputsFn { inherit root; };
+  flakeLockAndInputs = flakeLock // importAtomArgs.inputs;
+
+  # Nix flakes already have their inputs
+  flakeLockInputs = if _calledFromFlake then importAtomArgs.inputs else flakeLockAndInputs;
+
+  inputs =
+    let
+      inputsIndex = {
+        npins = npinsInputs;
+        flake-lock = flakeLockInputs;
+        native = throwMissingNativeFetcher;
+      };
+    in
+    inputsIndex.${fetcher} or throwNonExistingFetcher;
+
+  mkInput = name: inputs.${name};
+
+  # TODO how to handle features?
+  mkAtom =
+    depName: depConfig:
+    let
+      name = depConfig.name or depName;
+      type = depConfig.type or "atom";
+      srcRoot = if type == "local" then root else inputs.${name};
+      # TODO this will obviously evolve
+      manifestFileName = "${name}@.toml";
+      manifest = "${srcRoot}/${manifestFileName}";
+      overrides = depConfig.inputOverrides or [ ];
+      depHasInputOverrides = overrides != [ ];
+      overrideInputs = builtins.getAttrs overrides inputs;
+      optionalOverrides = if depHasInputOverrides then overrideInputs else { };
+      optionalInputs = if propagate then inputs else optionalOverrides;
+      args = {
+        inputs = optionalInputs;
+        inherit
+          system
+          importAtom
+          core
+          flakeCompatFn
+          flakeInputsFn
+          propagate
+          ;
+      };
+    in
+    importAtom args manifest;
+
+  mkFlake =
+    depName: depConfig:
+    let
+      name = depConfig.name or depName;
+      inputOverrides = depConfig.inputOverrides or [ ];
+      depHasInputOverrides = inputOverrides != [ ];
+      input = inputs.${name};
+
+      flakeCompatResult = flakeCompatFn {
+        src = input;
+        inherit system;
+      };
+
+      overrides = builtins.getAttrs inputOverrides inputs;
+      resultWithoutOverrides = flakeCompatResult.defaultNix;
+      resultWithInputOverrides = resultWithoutOverrides.overrideInputs overrides;
+
+      flakeCompatFlake =
+        if depHasInputOverrides then resultWithInputOverrides else resultWithoutOverrides;
+
+      inputIsFlake = input._type == "flake";
+      possiblyRawFlake = if inputIsFlake then input else flakeCompatFlake;
+
+    in
+    if _calledFromFlake then possiblyRawFlake else flakeCompatFlake;
+
+  mkImport =
+    depName: depConfig:
+    let
+      name = depConfig.name or depName;
+      input = inputs.${name};
+      inputIsFlake = input._type == "flake";
+      srcRootFromFlake = if inputIsFlake then input.outPath else input;
+      rawSrcRoot = if _calledFromFlake then srcRootFromFlake else input;
+      rawSrc = "${rawSrcRoot}/${depConfig.subdir or ""}";
+      depArgs = depConfig.args or [ ];
+      depHasArgs = depArgs != [ ];
+
+      applyNextArg =
+        appliedFunction: nextArgument:
+        let
+          argsFromDeps = depConfig.argsFromDeps or true && builtins.isAttrs nextArgument;
+          intersectedArgument = nextArgument // (builtins.intersectAttrs nextArgument extern);
+        in
+        if argsFromDeps then appliedFunction intersectedArgument else appliedFunction nextArgument;
+
+      importedSrcWithArgs = builtins.foldl' applyNextArg (import rawSrc) depArgs;
+
+    in
+    if depHasArgs then importedSrcWithArgs else import rawSrc;
+
+  mkSrc = depName: depConfig: mkInput (depConfig.name or depName);
 
   meta = atom.meta or { };
 
