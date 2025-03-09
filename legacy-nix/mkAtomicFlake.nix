901017135c6e2e069d9eb61ef6bf3ad4ec8f8dbd
li <li@maisiliym.criome.me>
2025-03-09 12:03:14 +0100

    feat!: modified `mkAtomicFlake` to use a system input flake

--- legacy-nix/mkAtomicFlake.nix
+++ legacy-nix/mkAtomicFlake.nix
@@ -1,8 +1,14 @@
-inputs: manifest:
+rawInputs: manifest:
 let
-  mkCustomAtomicFlake = import ./mkCustomAtomicFlake.nix;
+  importAtom = import ../atom-nix/core/importAtom.nix;
+
+  inputsHaveSystem = builtins.hasAttr "system" rawInputs;
+
+  mkAtom = importAtom {
+    inputs = builtins.removeAttrs rawInputs [ "system" ];
+    system = if inputsHaveSystem then rawInputs.system.value else null;
+    _calledFromFlake = true;
+  };
 
 in
-mkCustomAtomicFlake {
-  inherit inputs manifest;
-}
+mkAtom manifest
